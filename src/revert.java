ee2d3835432888c9a5e34abdb937082b6eaf2efa
igm0313 <igm0313.handong.ac.kr>
2026-04-06 22:20:56 +0900

    Revert "revert_2"
    This reverts commit 4e0c25cc381749a71ae283054b3461c059900c3d.

--- src/revert.java
+++ src/revert.java
@@ -6,18 +6,12 @@ public class revert {
         int year,month,day;
         String birth;
         String name;
-        String male;
 
-        System.out.print("Input your name : ");
-        name = s.nextLine();
-        System.out.print("Input your birthday : ");
         birth = s.nextLine();
-        System.out.print("Input your Sexual : ");
-        male = s.nextLine();
+        name = s.nextLine();
         year = Integer.parseInt(birth)/10000;
         month = Integer.parseInt(birth)/100%100;
         day = Integer.parseInt(birth)%100;
 
-        System.out.printf("%s : %d, %d, %d, %s",name,year,month,day,male);
     }
 }
